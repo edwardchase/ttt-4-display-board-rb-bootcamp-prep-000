f6ff05366553067f2a5ccfbc54c12731741086b8
Edward Mixon <edwardchase@gatech.edu>
2018-06-07 00:49:47 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,5 +8,5 @@ def display_board(board)
   puts " #{board[6]} , | , #{board[7]} , | , #{board[98} "
 end
 
-board = [" "," "," "," "," "," "," "," "," "]
+board = [" X "," X "," X "," "," "," "," "," "," "]
 display_board(board)
