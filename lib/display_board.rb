8f5b2950ad71d893cac575a90b746d42fdeef09c
Edward Mixon <edwardchase@gatech.edu>
2018-06-07 01:01:50 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -9,25 +9,3 @@ def display_board(board)
 end
 
 board = [];
-
-
-board = ["X","X","X"," "," "," "," "," "," "]
-display_board(board)
-
-board = [" "," "," "," ","X"," "," "," "," "]
-display_board(board)
-
-board = ["O"," "," "," "," "," "," "," "," "]
-display_board(board)
-
-board = ["O"," "," "," ","X"," "," "," "," "]
-display_board(board)
-
-board = [" X "," X "," X "," "," "," "," "," "," "]
-display_board(board)
-
-board = [" "," "," "," "," "," ","O","O","O"]
-display_board(board)
-
-board = ["X"," "," "," ","X"," "," "," ","X"]
-display_board(board)
