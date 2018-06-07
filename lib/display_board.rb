df8e524fe5e29e2aa4d7e8a07df9ec551206e526
Edward Mixon <edwardchase@gatech.edu>
2018-06-07 00:46:29 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,8 +2,10 @@
 # out the current state.
 def display_board(board)
   puts " #{board[0]} , | , #{board[0]} , | , #{board[0]} "
-  puts "#{board[0]}, #{board[0]},#{board[0]}"
-  puts "#{board[0]}, #{board[0]},#{board[0]}"
+  puts "-----------"
+  puts " #{board[0]} , | , #{board[0]} , | , #{board[0]} "
+  puts "-----------"
+  puts " #{board[0]} , | , #{board[0]} , | , #{board[0]} "
 end
 
 
